a54283a82f6e045f285c812c02992801da2c92ad
denis <permwed@gmail.com>
2022-03-17 09:43:44 +0500

    add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+// Static
+import logo from "./static/logo.png";
+// Css
+import "./App.scss";
 
 function App() {
-  return <div className="App">123</div>;
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="header__logo">
+          <img src={logo} alt="logo" width={64} height={64} />
+        </div>
+        <div className="header__text">
+          <div className="header__text_title">Фотостудии Перми</div>
+          <div className="header__text_text">
+            Все фотостудии Перми на одном сайте
+          </div>
+        </div>
+      </div>
+      <div className="disctricts">123</div>
+      <div className="content">123</div>
+      <div className="bottom">123</div>
+    </div>
+  );
 }
 
 export default App;
